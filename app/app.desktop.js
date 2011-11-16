976c1c23f7c63b32c171dce09b6231ba8d2a5ee8
Thomas Bassetto <tbassetto@gmail.com>
2011-11-16 18:03:57 +0100

    Start work on the Desktop app

---
+++ app/app.desktop.js
@@ -0,0 +1,61 @@
+Joshfire.define(['joshfire/app', 'joshfire/class', './src/tree.data', './src/tree.ui.desktop', 'joshfire/utils/splashscreen', 'joshfire/uielements/panel'], function(BaseApp, Class, Data, UI, Splash, Panel) {
+
+  return Class(BaseApp, {
+
+    id: 'defaultApp',
+
+    uiClass: UI,
+
+    dataClass: Data,
+
+    setup: function(callback) {
+      var _this = this,
+          menu = _this.ui.element('/menu'),
+          itemList = _this.ui.element('/content/itemList'),
+          detailPanel = _this.ui.element('/content/detail'),
+          title = _this.ui.element('/header/title');
+
+      this.splash = new Splash();
+
+      // Populate menu
+      menu.subscribe('data', function(event, data) {
+        // We have data, remove splashscreen
+        // Todo: it may better to remove it "onAfterShow" or "onAfterInsert"
+        _this.splash.remove();
+
+        // Select the first tab on our menu
+        menu.selectByIndex(0);
+      });
+
+      // Handle clicks on menu
+      menu.subscribe('select', function(event, data) {
+        var datasourceId = data[0][0];
+
+        // Set the new dataPath
+        _this.ui.element('/content/itemList').setDataPath('/datasource/' + datasourceId + '/');
+
+        // Show itemList, because we could be on a detail panel
+        _this.ui.element('/content').switchTo('itemList');
+      });
+
+      // Refresh content each time new data is attached
+      itemList.subscribe('data', function(event, data) {
+        itemList.refresh();
+      });
+
+      // Open item when selected
+      itemList.subscribe('select', function(event, data) {
+        _this.ui.element('/content/detail').show();
+      });
+
+      // Todo: find better event to select first item
+      itemList.subscribe('afterRefresh', function(event, data) {
+        // itemList.selectByIndex(0);
+      });
+
+      callback(null, true);
+    }
+
+  });
+
+});
