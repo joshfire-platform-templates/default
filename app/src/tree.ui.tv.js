986532675599ffbdbca6db303d07170bfce4e8c4
Thomas Bassetto <tbassetto@gmail.com>
2011-11-17 15:53:02 +0100

    Forgot this file

---
+++ app/src/tree.ui.tv.js
@@ -0,0 +1,72 @@
+Joshfire.define(['joshfire/class', 'joshfire/tree.ui', 'joshfire/uielements/list', 'joshfire/uielements/panel', 'joshfire/uielements/panel.manager', 'joshfire/uielements/button'], function(Class, UITree, List, Panel, PanelManager, Button) {
+
+  return Class(UITree, {
+
+    buildTree: function() {
+
+      var app = this.app;
+
+      app.currentDatasource = false;
+
+      return [
+        {
+          id: 'header',
+          type: Panel,
+          children: [
+            {
+              id: 'title',
+              type: 'Panel',
+              content: 'My App'
+            }
+          ]
+        },
+        {
+          id: 'content',
+          type: PanelManager,
+          uiMaster: '/footer',
+          autoShow: false,
+          children: [
+            {
+              id: 'itemList',
+              type: List,
+              orientation: 'left',
+              loadingTemplate: '<div>Loading itemList...</div>',
+              itemInnerTemplate:
+                '<div class="clearfix">'+
+                '    <p class="title"><%= item.title %></p>'+
+                '</div>'
+            },
+            {
+              id: 'detail',
+              type: Panel,
+              loadingTemplate: '<div>Loading detail...</div>',
+              uiDataMaster: '/content/itemList',
+              forceDataPathRefresh: true,
+              innerTemplate: // Dirty… TODO: need to know before if it's a picture, a video, etc.
+                '<h4 class="title"><%= data.title %></h4>'+
+                '<% if (data.image) { %>'+
+                '  <img style="max-width:100%" src="<%= data.image %>">'+
+                '<% } %>'+
+                '<% if (data.video) { %>'+
+                '  <p><a href="<%= data.video %>"><%= data.video %></a></p>'+
+                '<% } else { %>'+
+                '  <p><a href="<%= data.link %>"><%= data.link %></a></p>'+
+                '<% } %>'+
+                '<p>Par <strong><%= data.creator || data.user %></strong></p>'
+            }
+            
+          ]
+        },
+        {
+          id: 'footer',
+          type: List,
+          dataPath: '/datasourcelist/',
+          itemInnerTemplate: '<%= item.name %>',
+          onData: function() {} // trigger data, WTF?
+        }
+      ];
+    }
+
+  });
+
+});
